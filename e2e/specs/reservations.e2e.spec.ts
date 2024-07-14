074ba5b8a020c0cea4d26c0db9cb9b7abe6d4903
fordelord <79473861+fordelord@users.noreply.github.com>
2024-07-14 19:05:51 +0300

    Change ingress.yaml file

--- e2e/specs/reservations.e2e.spec.ts
+++ e2e/specs/reservations.e2e.spec.ts
@@ -5,13 +5,25 @@ describe('Reservations', () => {
       password: '!StrongPassword123',
     };
 
-    await fetch('http://auth:3002', {
+    await fetch('http://auth:3002/users', {
       method: 'POST',
       body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
+    const response = await fetch('http://auth:3002/auth/login', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const jwt = await response.text();
+    console.log('jwt', jwt);
   });
 
   test('Create', () => {
-    console.log();
+    expect(true).toBeTruthy();
   });
 });
